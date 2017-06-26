d76e848fc5792d7bc061501d930d2fd2bdabd590
swapnil-winjit <swapnilp@winjit.com>
2017-06-26 19:31:57 +0530

    - Signup screen - Contact us screen design completed

---
+++ FoodBuddies/Application Layer/Controllers/ContactUsVC/ContactUSViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ContactUSViewController.swift
+//  FoodBuddies
+//
+//  Created by Suraj Sonawane on 26/06/17.
+//  Copyright © 2017 demo. All rights reserved.
+//
+
+import UIKit
+
+class ContactUSViewController: UIViewController {
+
+    
+    
+//TODO: - General
+    
+    
+//TODO: - Controls
+    
+    @IBOutlet weak var viewAddress: UIView!
+    
+    @IBOutlet weak var viewMap: UIView!
+    
+    @IBOutlet weak var lblCopyright: UILabel!
+    
+//TODO: - Let's Code
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+//TODO: - UIButton Action
+   
+    @IBAction func btnBackClick(_ sender: Any) {
+    }
+
+}
